6294d5525a0d69712926b4112e35d1f66cb097fd
Corey Farwell <coreyf@rwell.org>
2015-06-30 20:59:07 -0700

    Utility doesn't need to be public

--- src/proj.rs
+++ src/proj.rs
@@ -4,7 +4,7 @@ use geo::{Point, Coordinate};
 use std::ffi::CStr;
 use std::str;
 
-pub fn _string(raw_ptr: *const c_char) -> String {
+fn _string(raw_ptr: *const c_char) -> String {
     let c_str = unsafe { CStr::from_ptr(raw_ptr) };
     return str::from_utf8(c_str.to_bytes()).unwrap().to_string();
 }
