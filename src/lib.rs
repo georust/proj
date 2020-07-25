feb1d5249b9ae78e150b85df26160b3d40e415ce
Stephan Hügel <shugel@tcd.ie>
2020-07-25 18:28:30 +0100

    Fix doc links

--- src/lib.rs
+++ src/lib.rs
@@ -19,29 +19,29 @@
 //! 1. If you don't require additional grids or other customisation:
 //!     - Call `Proj::new` or `Proj::new_known_crs`. This creates a transformation instance ([`Proj`](proj/struct.Proj.html))
 //! 2. If you require a grid for the transformation you wish to carry out, or you need to customise the search path or the grid endpoint:
-//!     - Create a new [`TransformBuilder`](proj/struct.TransformBuilder.html) by calling `TransformBuilder::new()`. It may be modified to enable network downloads, disable the grid, cache or modify search paths;
-//!     - Call [`TransformBuilder.proj()`](proj/struct.TransformBuilder.html#method.proj) or [`TransformBuilder.transform_known_crs()`](proj/struct.TransformBuilder.html#method.transform_known_crs). This creates a transformation instance (`Proj`)
+//!     - Create a new [`ProjBuilder`](proj/struct.ProjBuilder.html) by calling `ProjBuilder::new()`. It may be modified to enable network downloads, disable the grid, cache or modify search paths;
+//!     - Call [`ProjBuilder.proj()`](proj/struct.ProjBuilder.html#method.proj) or [`ProjBuilder.transform_known_crs()`](proj/struct.ProjBuilder.html#method.transform_known_crs). This creates a transformation instance (`Proj`)
 //!
 //! **Note**:
 //!
-//! 1. Both `TransformBuilder` and `Proj` implement the [`Info`](proj/trait.Info.html) trait, which can be used to get information about the current state of the `PROJ` instance;
-//! 2. `Proj::new()` and `TransformBuilder::proj()` have the same signature;
-//! 3. `Proj::new_known_crs()` and `TransformBuilder::transform_known_crs()` have the same signature.
+//! 1. Both `ProjBuilder` and `Proj` implement the [`Info`](proj/trait.Info.html) trait, which can be used to get information about the current state of the `PROJ` instance;
+//! 2. `Proj::new()` and `ProjBuilder::proj()` have the same signature;
+//! 3. `Proj::new_known_crs()` and `ProjBuilder::transform_known_crs()` have the same signature.
 //!
 //! ## Network, Cache, and Search Path Functionality
 //!
 //! ### Grid File Download
 //! `proj` supports [network grid download](https://proj.org/usage/network.html) functionality.
 //! Network access is **disabled** by default, and
-//! can be activated by passing a `true` `bool` to [`enable_network()`](proj/struct.Context.html#method.enable_network).
+//! can be activated by passing a `true` `bool` to [`enable_network()`](proj/struct.ProjBuilder.html#method.enable_network).
 //! Network functionality status can be queried with
 //! `network_enabled`, and the download endpoint can be queried and set using `get_url_endpoint` and `set_url_endpoint`.
 //!
 //! #### Grid File Cache
-//! Up to 300 mb of downloaded grids are cached to save bandwidth: This cache can be enabled or disabled using [`grid_cache_enable`](proj/struct.Context.html#method.grid_cache_enable).
+//! Up to 300 mb of downloaded grids are cached to save bandwidth: This cache can be enabled or disabled using [`grid_cache_enable`](proj/struct.ProjBuilder.html#method.grid_cache_enable).
 //!
 //! ### Search Path Modification
-//! The path used to search for resource files can be modified using [`set_search_paths`](proj/struct.Context.html#method.set_search_paths)
+//! The path used to search for resource files can be modified using [`set_search_paths`](proj/struct.ProjBuilder.html#method.set_search_paths)
 //!
 //!
 //! # Requirements
@@ -81,7 +81,7 @@ mod network;
 mod proj;
 
 pub use crate::proj::Area;
-pub use crate::proj::TransformBuilder;
+pub use crate::proj::ProjBuilder;
 pub use crate::proj::Info;
 pub use crate::proj::Proj;
 pub use crate::proj::ProjError;
